5fad3f94893211705fcc6738c6e956e2dac1a74b
EsziL <soos.balint1234@gmail.com>
2024-11-03 02:01:57 +0100

    fixed /dfutils not working by removing it

--- src/main/java/com/eszil/commands/CommandRegistry.java
+++ src/main/java/com/eszil/commands/CommandRegistry.java
@@ -6,7 +6,6 @@ import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 public class CommandRegistry {
 
     public static void registerCommands(CommandDispatcher<FabricClientCommandSource> dispatcher) {
-        dfutils.register(dispatcher);
         ip.register(dispatcher);
     }
 }
